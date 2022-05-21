692d372c37532184940ac4e5e8865e7fa1182388
Debora Buzon <debora.buzon@gmail.com>
2022-05-21 20:27:00 -0300

    Add mock function to get response from api

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const chat = document.getElementsByClassName("chat")[0]
 const userInput = document.querySelector("input");
 const sendBtn = document.getElementsByClassName("send")[0];
+let lastMessages = ["Hello, I’m TsunderAI-chan. You can ask me some questions, but I don’t have much time. Be quick! 😤"]
 
-const createChatBubble = (message) => {
-    let chatBubble = '<div class="chat-bubble user">'+ message +'</div>'
+const createChatBubble = (message, type) => {
+    let chatBubble;
+    if (type === "user") chatBubble = '<div class="chat-bubble user">'+ message +'</div>'
+    else chatBubble = '<div class="chat-bubble tsundere">'+ message +'</div>'
     chat.innerHTML += chatBubble
+}
+
+const getResponse = async (message) => {
+    if (lastMessages.length == 3) lastMessages.shift();
+    lastMessages.push(message);
+    const response = await setTimeout(() => {return "ok";}, 2000)
+    createChatBubble(response, "tsundere");
 }
 
 const sendMessage = () => {
     let message = userInput.value;
     if (!message) alert("Please type something before hitting the send button!");
     else {
-        createChatBubble(message);
+        createChatBubble(message, "user");
+        getResponse(message);
         userInput.value = "";
     }
 }
